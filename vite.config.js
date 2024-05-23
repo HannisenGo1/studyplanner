ba7bf5df0fa423996b1329ae453b85cf3393505a
HannisenGo1 <hannakarlsson69@gmail.com>
2024-05-23 20:57:59 +0200

    hallaaa

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
     server: {
         host: true,
         port: 5555,
-        cors: true // Lägg till denna rad för att aktivera CORS  tillåta åtkomst 
+        cors: true 
     }
 })
