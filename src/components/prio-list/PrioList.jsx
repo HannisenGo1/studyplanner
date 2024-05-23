48d7ece5a73a8b61b060a86b6e1ce579e2cebc49
HannisenGo1 <hannakarlsson69@gmail.com>
2024-05-23 17:54:49 +0200

    filtrering Priolistan är klar

--- src/components/prio-list/PrioList.jsx
+++ src/components/prio-list/PrioList.jsx
@@ -1,38 +1,38 @@
 import React, { useState } from 'react';
 import PrioItem from './PrioItem';
 import { useStore } from '../../data/store.js';
-
-// TO-DO: göra så att filtreringen blir kopplat till todo/items i ordning beroende på dag.
-// INTE KLAR !!!!
+import { getToday } from '../../utils/date.js';
 
 const PrioList = () => {
   const todos = useStore(state => state.todos);
   const [searchText, setSearchText] = useState('');
-  
-  // Filtrera todo-items baserat på söktexten
-  const filteredItems = todos.filter(item =>
-    item.text.toLowerCase().includes(searchText.toLowerCase())
-  );
-  
+  const today = getToday();
+
+  const filteredAndSortedItems = todos
+    .filter(item => item.text.toLowerCase().includes(searchText.toLowerCase()))
+    .sort((a, b) => {
+      // todo-items med dagens datum och sortera i stigande ordning
+      const dayOrder = ['måndag', 'tisdag', 'onsdag', 'torsdag', 'fredag', 'lördag', 'söndag'];
+      return dayOrder.indexOf(a.day) - dayOrder.indexOf(b.day);
+    });
+
   return (
     <div className="prio-list">
-    <h2>Vad ska jag göra nu?</h2>
-    <div className="list-container">
-    
-    <input
-    type="search"
-    placeholder="Filtrera uppgifter"
-    value={searchText}
-    onChange={(e) => setSearchText(e.target.value)}
-    />
-    
-    <div className="prio-items">
-    {/* Rendera filtrerade todo-items */}
-    {filteredItems.map((item, index) => (
-      <PrioItem key={`${item.id}-${index}`} item={item} num={index + 1} />
-    ))}
-    </div>
-    </div>
+      <h2>Vad ska jag göra nu?</h2>
+      <div className="list-container">
+        <input
+          type="search"
+          placeholder="Filtrera uppgifter"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <div className="prio-items">
+        
+          {filteredAndSortedItems.map((item, index) => (
+            <PrioItem key={`${item.id}-${index}`} item={item} num={index + 1} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
